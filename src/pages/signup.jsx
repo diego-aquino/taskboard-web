2277686de254ad4c4a42a5c809a7d499ff55166f
Diego Aquino <diego.cruz001@gmail.com>
2021-04-26 10:10:36 -0300

    Abstract sign up form to an independent component

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,95 +1,45 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { useCallback, useRef, useState } from 'react';
-import * as yup from 'yup';
+import { useCallback, useRef } from 'react';
 
 import api from '~/api';
 import { AbstractSquares, PersonWithPosters } from '~/assets';
-import { Alert, Button, Input } from '~/components/common';
+import { SignUpForm } from '~/components/homePage';
 import styles from '~/styles/pages/SignUpPage.module.scss';
 import * as errors from '~/utils/errors';
-import * as validate from '~/utils/validation';
-
-const { ValidationError } = yup;
 
 const SignUpPage = () => {
-  const inputRefs = {
-    firstName: useRef(null),
-    lastName: useRef(null),
-    email: useRef(null),
-    password: useRef(null),
-    confirmPassword: useRef(null),
-  };
-  const [emailCustomAlertMessage, setEmailCustomAlertMessage] = useState(null);
-  const [globalAlertMessage, setGlobalAlertMessage] = useState(null);
+  const signUpFormRef = useRef(null);
 
-  const validatePassword = useCallback(async (password) => {
-    await validate.requiredPasswordField(password, 8);
+  const registerAccount = useCallback(async (accountInfo) => {
+    await api.post('/accounts/signup', accountInfo);
   }, []);
-
-  const validateConfirmedPassword = useCallback(
-    async (confirmedPassword) => {
-      await validate.requiredTextField(confirmedPassword);
-
-      const password = inputRefs.password.current?.value;
-      const passwordsDidMatch = password === confirmedPassword;
-
-      if (!passwordsDidMatch) {
-        throw new ValidationError('As senhas não conferem');
-      }
-    },
-    [inputRefs.password],
-  );
 
   const handleSignUpError = useCallback((error) => {
     const errorType = errors.getNetworkErrorType(error.response);
     const message = errors.generateNetworkErrorMessage(errorType);
 
+    const { setCustomAlertMessage } = signUpFormRef.current || {};
+
     switch (errorType) {
       case errors.types.EMAIL_ALREADY_IN_USE: {
-        return setEmailCustomAlertMessage(message);
+        return setCustomAlertMessage?.({ email: message });
       }
       default:
-        return setGlobalAlertMessage(message);
+        return setCustomAlertMessage?.({ global: message });
     }
   }, []);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    const inputElements = Object.values(inputRefs).map((ref) => ref.current);
-
-    const validationResults = await Promise.all(
-      inputElements.map((element) => element.validate()),
-    );
-    const allFieldsAreValid = validationResults.every(
-      (result) => result === true,
-    );
-    if (!allFieldsAreValid) return;
-
-    const [firstName, lastName, email, password] = [
-      inputRefs.firstName,
-      inputRefs.lastName,
-      inputRefs.email,
-      inputRefs.password,
-    ].map((ref) => ref.current?.value);
-
-    try {
-      await api.post('/accounts/signup', {
-        firstName,
-        lastName,
-        email,
-        password,
-      });
-    } catch (error) {
-      handleSignUpError(error);
-    }
-  };
-
-  const preventSubmitByPressingEnter = useCallback((event) => {
-    if (event.key !== 'Enter') return;
-    event.preventDefault();
-  }, []);
+  const handleValidFormSubmit = useCallback(
+    async ({ firstName, lastName, email, password }) => {
+      try {
+        await registerAccount({ firstName, lastName, email, password });
+      } catch (error) {
+        handleSignUpError(error);
+      }
+    },
+    [registerAccount, handleSignUpError],
+  );
 
   return (
     <div className={styles.container}>
@@ -97,88 +47,24 @@ const SignUpPage = () => {
         <title>Registrar-se | Taskboard</title>
       </Head>
 
-      <div className={styles.decorativeSectionContainer}>
+      <section className={styles.decorativeSectionContainer}>
         <AbstractSquares className={styles.decorativeSquares} />
         <AbstractSquares className={styles.decorativeSquares} />
         <AbstractSquares className={styles.decorativeSquares} />
         <PersonWithPosters className={styles.personWithPosters} />
-      </div>
+      </section>
 
-      <div className={styles.signUpSection}>
+      <section className={styles.signUpSection}>
         <h1>Registre-se</h1>
         <p>E gerencie as suas tarefas com eficiência!</p>
 
-        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-        <form
-          onSubmit={handleFormSubmit}
-          onKeyDown={preventSubmitByPressingEnter}
-        >
-          <div className={styles.twoColumnInputContainer}>
-            <Input
-              ref={inputRefs.firstName}
-              type="text"
-              name="firstName"
-              label="Nome"
-              validate={validate.requiredTextField}
-              required
-            />
-            <Input
-              ref={inputRefs.lastName}
-              type="text"
-              name="lastName"
-              label="Sobrenome"
-              validate={validate.requiredTextField}
-              required
-            />
-          </div>
-          <Input
-            ref={inputRefs.email}
-            type="email"
-            name="email"
-            label="Email"
-            placeholder="nome@dominio.com"
-            validate={validate.requiredEmailField}
-            customAlertMessage={emailCustomAlertMessage}
-            required
-          />
-          <div className={styles.twoColumnInputContainer}>
-            <Input
-              ref={inputRefs.password}
-              type="password"
-              name="password"
-              label="Senha"
-              placeholder="********"
-              minLength={8}
-              validate={validatePassword}
-              required
-            />
-            <Input
-              ref={inputRefs.confirmPassword}
-              type="password"
-              name="confirmPassword"
-              label="Confirmar senha"
-              placeholder="********"
-              minLength={8}
-              validate={validateConfirmedPassword}
-              required
-            />
-          </div>
-
-          {globalAlertMessage && (
-            <Alert
-              className={styles.globalAlertContainer}
-              message={globalAlertMessage}
-            />
-          )}
-
-          <Button type="submit">Registrar</Button>
-        </form>
+        <SignUpForm ref={signUpFormRef} onValidSubmit={handleValidFormSubmit} />
 
         <div className={styles.alreadyRegisteredContainer}>
           <span>Já tem uma conta?</span>
           <Link href="/login">Faça login</Link>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
