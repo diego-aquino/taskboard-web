1a9b250cad1a96804e19a8fd90c410420846cd68
Murilo <murilo.castro@ccc.ufcg.edu.br>
2021-04-26 01:21:41 -0300

    Create login page layout

---
+++ src/pages/login.jsx
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+
+import { Input, Button } from '~/components/common';
+import styles from '~/styles/pages/LoginPage.module.scss';
+
+import { AbstractSquares, PersonWithSmartphone } from '../assets';
+
+const Login = () => (
+  <div className={styles.container}>
+    <Head>
+      <title>Login | Taskboard</title>
+    </Head>
+
+    <section className={styles.loginSection}>
+      <h1>Login</h1>
+      <p>Seja bem vindo de volta!</p>
+      <form>
+        <Input
+          name="email"
+          label="Email"
+          type="email"
+          placeholder="nome@dominio.com"
+          required
+        />
+        <Input
+          name="password"
+          label="Senha"
+          type="password"
+          placeholder="********"
+          minLength={8}
+          required
+        />
+        <Button type="submit">Login</Button>
+        <div className={styles.center}>
+          <span>
+            Não tem uma conta? <a href="">Registre-se</a>
+          </span>
+        </div>
+      </form>
+    </section>
+    <section className={styles.decorativeSection}>
+      <AbstractSquares className={styles.decorativeAbstractSquares} />
+      <AbstractSquares className={styles.decorativeAbstractSquares} />
+      <AbstractSquares className={styles.decorativeAbstractSquares} />
+      <PersonWithSmartphone className={styles.PersonWithSmartphone} />
+    </section>
+  </div>
+);
+
+export default Login;
