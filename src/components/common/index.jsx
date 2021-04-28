0571e0c92496950557796b3019cd62631f8a2d8b
Diego Aquino <diego.cruz001@gmail.com>
2021-04-28 18:47:40 -0300

    Implement component to render modals on screen

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -1,6 +1,7 @@
 import Alert from './Alert';
 import Button from './Button';
 import Input from './Input';
+import Modal from './Modal';
 import SwitchButton from './SwitchButton';
 
-export { Input, Button, Alert, SwitchButton };
+export { Input, Button, Alert, SwitchButton, Modal };
