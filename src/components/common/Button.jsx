9cf5154f65c3866929c1d8b4658628ffdf9207d7
Diego Aquino <diego.cruz001@gmail.com>
2021-04-29 11:22:13 -0300

    Add support to show a loading sign in buttons

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,10 +1,17 @@
 import clsx from 'clsx';
 
+import { LoadingIcon } from '~/assets';
 import styles from '~/styles/components/common/Button.module.scss';
 
-const Button = ({ className, children, ...rest }) => (
-  <button className={clsx(styles.container, className)} type="button" {...rest}>
-    {children}
+const Button = ({ loading: isLoading, className, children, ...rest }) => (
+  <button
+    className={clsx(styles.container, isLoading && styles.loading, className)}
+    type="button"
+    disabled={isLoading}
+    {...rest}
+  >
+    <div className={styles.childrenContainer}>{children}</div>
+    {isLoading && <LoadingIcon />}
   </button>
 );
 
